0639c5c43bf9ee871d79fd2251a12c0e75d901c4
Krzysztof Ćmiel <cmielkrzysztof@gmail.com>
2024-10-17 12:33:06 +0200

    added swagger ui

--- src/main/java/com/example/jwt/demo/DemoController.java
+++ src/main/java/com/example/jwt/demo/DemoController.java
@@ -1,5 +1,9 @@
 package com.example.jwt.demo;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +11,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/demo-controller")
+@Tag(name = "Demo")
 public class DemoController {
 
+    @Operation(
+            description = "Get endpoint for say hello",
+            summary = "This is summary for demo get endpoint",
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "Unauthorized / Invalid Token",
+                            responseCode = "403"
+                    )
+            }
+    )
     @GetMapping
     public ResponseEntity<String> sayHello() {
         return ResponseEntity.ok("Hello from secured endpoint");
